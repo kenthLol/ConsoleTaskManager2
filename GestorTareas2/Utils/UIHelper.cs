1c46cc74f759aa9ebeeff20d78fc46ec7130a046
kenthLol <kenthsamu@gmail.com>
2025-12-04 15:40:57 -0600

    feat: Add UIHelper class with PrintList utility method
    A new static `UIHelper` class has been added to the `GestorTareas2.Utils` namespace to provide utility methods. The class includes a `PrintList<T>` method that prints a collection of items to the console with a 1-based index. If the collection is empty, it prints a specified message instead.

---
+++ GestorTareas2/Utils/UIHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestorTareas2.Utils
+{
+    public static class UIHelper
+    {
+        /// <summary>
+        /// Prints each item in the specified collection to the console. If the collection is empty, prints the provided
+        /// message instead.
+        /// </summary>
+        /// <typeparam name="T">The type of elements contained in the collection to print.</typeparam>
+        /// <param name="items">The collection of items to print to the console. If the collection is empty, no items are printed.</param>
+        /// <param name="emptyMessage">The message to print to the console if the collection contains no items. Cannot be null.</param>
+        public static void PrintList<T>(IEnumerable<T> items, string emptyMessage)
+        {
+            var list = items.ToList();
+
+            if(list.Count > 0)
+            {
+                for(int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {list[i]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine(emptyMessage);
+            }
+        }
+    }
+}
